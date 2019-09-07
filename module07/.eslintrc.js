3df2ffb1c9efcd8bda9f3ebe562ceb507d606c5d
wellyngtonAlvs <wellington.02@icloud.com>
2019-09-07 20:46:38 -0300

    Added hooks

--- module07/.eslintrc.js
+++ module07/.eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -35,6 +36,9 @@ module.exports = {
     'no-console': ["error", { allow: ["tron"] }],
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
